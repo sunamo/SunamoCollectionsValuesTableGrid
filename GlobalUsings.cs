568964ad5195d3e241020424d42d419dd9e2c4a5
sunamo.cz <radek.jancik@sunamo.cz>
2025-03-29 19:04:10 +0100

    25.3.29.1 Updated nuget to latest versions

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -14,7 +14,6 @@ global using SunamoCollectionsValuesTableGrid;
 global using SunamoCollectionsValuesTableGrid._sunamo;
 global using SunamoCollectionsValuesTableGrid._sunamo.SunamoArgs;
 global using SunamoCollectionsValuesTableGrid._sunamo.SunamoInterfaces.Interfaces;
-
 global using ILogger = Microsoft.Extensions.Logging.ILogger;
 global using Microsoft.Extensions.Logging;
 global using NullLogger = Microsoft.Extensions.Logging.Abstractions.NullLogger;
