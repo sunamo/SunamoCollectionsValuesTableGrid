7fcf9fc8ba6c143c06b32b017df92a8a8580e4e7
Radek Jancik <radek.jancik@usys.cz>
2025-12-31 01:05:39 +0100

    25.12.31.1 feat: better naming for variables

--- SunamoCollectionsValuesTableGrid/_sunamo/FS.cs
+++
@@ -1,9 +0,0 @@
-// EN: Variable names have been checked and replaced with self-descriptive names
-// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
-
-namespace SunamoCollectionsValuesTableGrid._sunamo;
-
-//namespace SunamoCollectionsValuesTableGrid._sunamo.SunamoExceptions._AddedToAllCsproj;
-internal class FS
-{
-}
--- SunamoCollectionsValuesTableGrid/_sunamo/SH.cs
+++
@@ -1,8 +0,0 @@
-// EN: Variable names have been checked and replaced with self-descriptive names
-// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
-
-namespace SunamoCollectionsValuesTableGrid._sunamo;
-
-internal class SH
-{
-}
