cc891d3fcd4e53e6aa12b298f9a239092a5350c3
Radek Jancik <radek.jancik@usys.cz>
2026-02-04 12:20:26 +0100

    26.2.4.1 feat: Enable SourceLink and debug symbols

--- SunamoCollectionsValuesTableGrid/_sunamo/SunamoInterfaces/Interfaces/IValuesTableGrid.cs
+++ SunamoCollectionsValuesTableGrid/_sunamo/SunamoInterfaces/Interfaces/IValuesTableGrid.cs
@@ -1,4 +1,3 @@
-// variables names: ok
 namespace SunamoCollectionsValuesTableGrid._sunamo.SunamoInterfaces.Interfaces;
 
 // EN: Variable names have been checked and replaced with self-descriptive names
