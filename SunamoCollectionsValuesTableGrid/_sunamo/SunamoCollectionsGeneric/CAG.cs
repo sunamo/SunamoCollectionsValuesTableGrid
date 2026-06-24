e5df655dca1e8b922bc5a0a7076d0119fcebb1b1
smutekutek <smutekutek@gmail.com>
2026-06-24 22:34:29 +0200

    Ahoj! Jak ti mohu pomoct?

--- SunamoCollectionsValuesTableGrid/_sunamo/SunamoCollectionsGeneric/CAG.cs
+++ SunamoCollectionsValuesTableGrid/_sunamo/SunamoCollectionsGeneric/CAG.cs
@@ -1,16 +1,7 @@
 namespace SunamoCollectionsValuesTableGrid._sunamo.SunamoCollectionsGeneric;
 
-/// <summary>
-/// Collection aggregate generic helper methods for working with nested lists.
-/// </summary>
 internal class CAG
 {
-    /// <summary>
-    /// Finds the minimum number of elements among all inner lists.
-    /// </summary>
-    /// <typeparam name="T">The type of elements in the lists.</typeparam>
-    /// <param name="lists">The collection of lists to analyze.</param>
-    /// <returns>The minimum count of elements found in any inner list.</returns>
     internal static int MinElementsItemsInnerList<T>(List<List<T>> lists)
     {
         var min = int.MaxValue;
@@ -22,12 +13,6 @@ internal class CAG
         return min;
     }
 
-    /// <summary>
-    /// Finds the maximum number of elements among all inner lists.
-    /// </summary>
-    /// <typeparam name="T">The type of elements in the lists.</typeparam>
-    /// <param name="lists">The collection of lists to analyze.</param>
-    /// <returns>The maximum count of elements found in any inner list.</returns>
     internal static int MaxElementsItemsInnerList<T>(List<List<T>> lists)
     {
         var max = 0;
@@ -39,12 +24,6 @@ internal class CAG
         return max;
     }
 
-    /// <summary>
-    /// Finds the lowest count of elements among all inner lists.
-    /// </summary>
-    /// <typeparam name="T">The type of elements in the lists.</typeparam>
-    /// <param name="lists">The collection of lists to analyze.</param>
-    /// <returns>The lowest count of elements found in any inner list.</returns>
     internal static int LowestCount<T>(List<List<T>> lists)
     {
         var min = int.MaxValue;
@@ -56,13 +35,6 @@ internal class CAG
         return min;
     }
 
-    /// <summary>
-    /// Trims all inner lists to a specified maximum count by taking only the first maxCount elements.
-    /// </summary>
-    /// <typeparam name="T">The type of elements in the lists.</typeparam>
-    /// <param name="lists">The collection of lists to trim.</param>
-    /// <param name="maxCount">The maximum number of elements to keep in each inner list.</param>
-    /// <returns>The modified collection with trimmed inner lists.</returns>
     internal static List<List<T>> TrimInnersToCount<T>(List<List<T>> lists, int maxCount)
     {
         for (var i = 0; i < lists.Count; i++) lists[i] = lists[i].Take(maxCount).ToList();
